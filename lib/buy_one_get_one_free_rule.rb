06825298d19d17faa88b4cdc15c5932a9ff99b09
Jason Liggi <jason@liggi.net>
2015-08-16 16:26:40 +0100

    Structure for BOGOF rule

--- lib/buy_one_get_one_free_rule.rb
+++ lib/buy_one_get_one_free_rule.rb
@@ -1,4 +1,11 @@
 class BuyOneGetOneFreeRule < BaseRule
+  attr_reader :codes
+
+  def initialize(product_codes)
+    @codes = Array.new
+    product_codes.kind_of?(Array) ? @codes.concat(product_codes) : @codes.push(product_codes)
+  end
+
   def apply(checkout, prices)
 
   end
--- spec/buy_one_get_one_free_rule_spec.rb
+++ spec/buy_one_get_one_free_rule_spec.rb
@@ -2,14 +2,38 @@ require 'base_rule'
 require 'buy_one_get_one_free_rule'
 
 describe BuyOneGetOneFreeRule do
+  context "when created" do
+    it "is a subclass of BaseRule" do
+      @rule = BuyOneGetOneFreeRule.new("")
+      expect(@rule).to be_kind_of(BaseRule)
+    end
+  end
+
   describe ".apply" do
-    context "when created" do
+    context "when given a single product code" do
       before :each do
-        @rule = BuyOneGetOneFreeRule.new
+        @rule = BuyOneGetOneFreeRule.new("product_1")
       end
 
-      it "is a subclass of BaseRule" do
-        expect(@rule).to be_kind_of(BaseRule)
+      it "contains a single product code" do
+        expect(@rule.codes).to include("product_1")
+      end
+    end
+
+    context "when given multiple product codes" do
+      before :each do
+        @rule = BuyOneGetOneFreeRule.new(["product_1", "product_2", "product_3"])
+      end
+
+      it "contains all specified product codes" do
+        expect(@rule.codes).to include("product_1") && include("product_2") && include("product_3")
+      end
+    end
+
+    context "when two applicable items are in checkout" do
+      it "cuts the price in half" do
+        @checkout = Checkout.new
+
       end
     end
   end
